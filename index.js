03a386907da178e2716b5825c066e879d4eddace
Rui <rzzprtmptr@gmail.com>
2021-04-21 14:32:01 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import "./assets/css/main.css";
+import "./assets/js/util.js";
+import "./assets/sass/main.scss";
+import main from "./assets/js/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
